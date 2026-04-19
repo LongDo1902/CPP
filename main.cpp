28a24321c7253eefc2c5ade1892546c2e95c61ba
LongDo1902 <dobaolong2002@gmail.com>
2026-04-19 12:23:43 +0700

    Fix vscode folder

--- main.cpp
+++ main.cpp
@@ -1,30 +1,99 @@
-#include "fraction/fraction.hpp"
+#include <initializer_list>
 #include <iostream>
 #include <stdio.h>
 
-using namespace std;
+class fraction_t {
+  public:
+    int num;
+    int den;
+};
 
-ostream &
-operator<<(ostream &os, fraction A)
+template <typename T> class array_fixed {
+  public:
+    T  *array_memory;
+    int num_elements;
+
+    array_fixed(int size) : num_elements(size)
+    {
+        array_memory = (T *)malloc(num_elements * sizeof(T));
+    }
+
+    array_fixed(std::initializer_list<T> init_list)
+    {
+        array_memory = (T *)malloc(init_list.size() * sizeof(T));
+        num_elements = init_list.size();
+
+        int i = 0;
+        for (auto list : init_list) {
+            array_memory[i++] = list;
+        }
+    }
+
+    // To use foreach, our class has to enable begin() and end()
+    // begin() return the first address
+    // end() return the last address
+    T *
+    begin()
+    {
+        return &array_memory[0];
+    }
+
+    T *
+    end()
+    {
+        return &array_memory[num_elements];
+    }
+
+    T &
+    operator[](int idx)
+    {
+        return array_memory[idx];
+    }
+
+    ~array_fixed()
+    {
+        if (array_memory != nullptr) {
+            free(array_memory);
+            array_memory = nullptr;
+        }
+    }
+
+    // Disable copying
+    array_fixed(const array_fixed &)            = delete;
+    array_fixed &operator=(const array_fixed &) = delete;
+};
+
+bool
+compare_fraction(const fraction_t &A, const fraction_t &B)
 {
-    os << "phan so: " << A.num << "/" << A.den;
-    return os;
+    // Convert to decimal before comparing
+    return ((double)A.num / A.den) > ((double)B.num / B.den);
+}
+
+template <typename T>
+void
+arr_accending_order(array_fixed<T> &arr)
+{
+    int size = arr.num_elements;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (compare_fraction(arr[i], arr[j])) {
+                T temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
 
 int
 main()
 {
-    fraction A = {1, 2};
-    fraction B = {2, 3};
-
-    fraction mult_fraction = A * B;
-    fraction sum_fraction  = A + B;
-
-    mult_fraction.Simplify();
-    sum_fraction.Simplify();
-
-    cout << mult_fraction << endl;
-    cout << sum_fraction << endl;
-
+    array_fixed<fraction_t> arrA{{1, 2}, {2, 3}};
+    arr_accending_order(arrA);
+    for (auto i : arrA) {
+        std::cout << i.num << "/" << i.den << "\n";
+    }
     return 0;
 }
