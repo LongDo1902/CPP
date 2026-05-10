94981c08d8915221bb4591557d962062f19f0ffe
LongDo1902 <dobaolong2002@gmail.com>
2026-05-10 15:46:42 +0700

    Q2

--- main.cpp
+++ main.cpp
@@ -1,99 +1,64 @@
+#include <cmath>
+#include <complex>
 #include <initializer_list>
 #include <iostream>
 #include <stdio.h>
 
-class fraction_t {
+// ax^2 + bx + c = 0
+using namespace std;
+
+class QuadraticSolver {
+  private:
+    double a, b, c;
+
   public:
-    int num;
-    int den;
+    // Constructor
+    QuadraticSolver(double a, double b, double c) : a(a), b(b), c(c) {}
+
+    double
+    Get_Discriminant() const
+    {
+        double D = (b * b) - (4 * a * c);
+        return D;
+    };
+
+    void
+    Quadratic_Solver(double *x1, double *x2)
+    {
+        // Check NULL
+        if (!x1 || !x2)
+            return;
+
+        // Check if a == 0 ? It is quadratic : Otherwise not
+        if (a == 0.0)
+            return;
+
+        double D = QuadraticSolver::Get_Discriminant();
+
+        // Check if D > 0 ? There are two real roots : There is one repeated root
+        if (D > 0.0) {
+            *x1 = (double)(b + sqrt(D)) / (2.0 * a);
+            *x2 = (double)(b - sqrt(D)) / (2.0 * a);
+        }
+
+        else if (D == 0.0) {
+            *x1 = (double)(-b / (2.0 * a));
+            *x2 = *x1;
+        }
+
+        else { // Complex root
+            double real_part    = -b / (2.0 * a);
+            double complex_part = sqrt(-D) / (2.0 * a);
+
+            complex<double> x1 = complex<double>(real_part, complex_part);
+            complex<double> x2 = complex<double>(real_part, -complex_part);
+        }
+    }
 };
-
-template <typename T> class array_fixed {
-  public:
-    T  *array_memory;
-    int num_elements;
-
-    array_fixed(int size) : num_elements(size)
-    {
-        array_memory = (T *)malloc(num_elements * sizeof(T));
-    }
-
-    array_fixed(std::initializer_list<T> init_list)
-    {
-        array_memory = (T *)malloc(init_list.size() * sizeof(T));
-        num_elements = init_list.size();
-
-        int i = 0;
-        for (auto list : init_list) {
-            array_memory[i++] = list;
-        }
-    }
-
-    // To use foreach, our class has to enable begin() and end()
-    // begin() return the first address
-    // end() return the last address
-    T *
-    begin()
-    {
-        return &array_memory[0];
-    }
-
-    T *
-    end()
-    {
-        return &array_memory[num_elements];
-    }
-
-    T &
-    operator[](int idx)
-    {
-        return array_memory[idx];
-    }
-
-    ~array_fixed()
-    {
-        if (array_memory != nullptr) {
-            free(array_memory);
-            array_memory = nullptr;
-        }
-    }
-
-    // Disable copying
-    array_fixed(const array_fixed &)            = delete;
-    array_fixed &operator=(const array_fixed &) = delete;
-};
-
-bool
-compare_fraction(const fraction_t &A, const fraction_t &B)
-{
-    // Convert to decimal before comparing
-    return ((double)A.num / A.den) > ((double)B.num / B.den);
-}
-
-template <typename T>
-void
-arr_accending_order(array_fixed<T> &arr)
-{
-    int size = arr.num_elements;
-
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (compare_fraction(arr[i], arr[j])) {
-                T temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-}
 
 int
 main()
 {
-    array_fixed<fraction_t> arrA{{1, 2}, {2, 3}};
-    arr_accending_order(arrA);
-    for (auto i : arrA) {
-        std::cout << i.num << "/" << i.den << "\n";
-    }
-    return 0;
+    QuadraticSolver solver(1.0, -5.0, 6.0);
+    cout << (double)solver.Get_Discriminant();
 }
